31f8a75dec7f2fee9984f287a1cc7b457d5921ba
John Fu <fu.johnyh@gmail.com>
2022-08-29 16:22:07 -0700

    loops

---
+++ fundamentals/fundamentals/notes.py
@@ -0,0 +1,94 @@
+# lists
+ninjas = ['Rozen', 'KB', 'Oliver']
+my_list = ['4', ['list', 'in', 'a', 'list'], 987]
+empty_list = []
+
+fruits = ['apple', 'banana', 'orange', 'strawberry']
+vegetables = ['lettuce', 'cucumber', 'carrots']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+salad = 3 * vegetables
+print(salad)
+
+# manipulating lists
+drawers = ["documents", "envelopes", "pens"]
+    
+# access the drawer with index of 0 and print value
+print(drawers[0])  #prints documents
+# access the drawer with index of 1 and print value
+print(drawers[1]) #prints envelopes
+# access the drawer with index of 2 and print value
+print(drawers[2]) #prints pens
+    
+# replace "documents" with "tchotchkes"
+drawers[0] = "tchotchkes"
+print(drawers) # prints ["tchotchkes", "envelopes", "pens"]
+    
+# stores the value "tchotchkes" in a temporary variable.
+top_contents = drawers[0]
+    
+# Replaces the value at index 1
+# with whatever value is stored at index 0
+drawers[1] = drawers[0]
+print(drawers) # prints ["tchotchkes", "tchotchkes", "pens"]
+
+nums = [1,2,3,4,5]
+nums.append(99)
+print(nums)
+#the output would be [1,2,3,4,5,99]
+
+words = ["start","going","till","the","end"]
+# Sub-ranges (portions) of the list
+print(words[1:]) # prints ['going', 'till', 'the', 'end']
+print(words[:4]) # prints ['start', 'going', 'till', 'the']
+print(words[2:4]) # prints ['till', 'the']
+    
+# Making a copy of a list
+copy_of_words = words[:]
+copy_of_words.append("dojo") # only appends to the copy
+print(copy_of_words) # prints ['start', 'going', 'till', 'the', 'end', 'dojo']
+print(words) # prints ['start', 'going', 'till', 'the', 'end']
+
+# built-in list functions
+my_list = [1, 'Zen', 'hi']
+print(len(my_list))
+# output: 3
+
+my_list = [1,5,2,8,4]
+my_list.pop()
+print(my_list)
+# output:
+# [1,5,2,8]'
+
+# append, pop, index, reverse, sort
+
+#################################################################
+# Tuples
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = "a", "b", "c", "d"
+
+########################################################################
+# Conditionals
+x = 12
+if x > 50:
+    print("bigger than 50")
+else:
+    print("smaller than 50")
+# because x is not greater than 50, the second print statement is the only one that will execute
+
+x = 55
+if x > 10:
+    print("bigger than 10")
+elif x > 50:
+    print("bigger than 50")
+else:
+    print("smaller than 10")
+# even though x is greater than 10 and 50, the first true statement is the only one that will execute, so we will only see 'bigger than 10'
+
+if x < 10:
+    print("smaller than 10")
+# nothing happens, because the statement is false   
+
+########################################################################
+# Loops
