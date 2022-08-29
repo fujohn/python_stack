1d03eb0c33ca4a8c4e6934e75276e26034c5e05f
John Fu <fu.johnyh@gmail.com>
2022-08-29 15:33:27 -0700

    recognize python

--- fundamentals/introduction/introduction.py
+++ fundamentals/introduction/introduction.py
@@ -4,3 +4,13 @@ x = "Hello Python"
 print(x)
 y = 42
 print(y)
+
+int_to_float = float(35)
+float_to_int = int(44.2)
+int_to_complex = complex(35)
+print(int_to_float)
+print(float_to_int)
+print(int_to_complex)
+print(type(int_to_float))
+print(type(float_to_int))
+print(type(int_to_complex))
