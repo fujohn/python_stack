fa9e0c635ea7056d0562041a55626d6048d927e2
John Fu <fu.johnyh@gmail.com>
2022-08-29 22:13:42 -0700

    functions basic i

--- fundamentals/fundamentals/notes.py
+++ fundamentals/fundamentals/notes.py
@@ -149,3 +149,42 @@ else:		# only executes on a clean exit from the while loop (i.e. not a break)
     print("Final else statement")
 # output: 3, 2, 1
 
+########################################################################
+# Functions
+def add(a,b):	# function name: 'add', parameters: a and b
+    x = a + b	# process
+    return x	# return value: x
+
+new_val = add(3, 5)    # calling the add function, with arguments 3 and 5
+print(new_val)    # the result of the add function gets sent back to and saved into new_val, so we will see 8
+
+def say_hi(name):
+    print("Hi, " + name)
+say_hi('Michael')
+say_hi('Anna')
+say_hi('Eli')
+
+def say_hi(name):
+    return "Hi " + name
+greeting = say_hi("Michael") # the returned value from say_hi function gets assigned to the 'greeting' variable
+print(greeting) # this will output 'Hi Michael'
+
+def add(a, b):
+    x = a + b
+    return x
+sum1 = add(4,6)
+sum2 = add(1,4)
+sum3 = sum1 + sum2
+print(sum3)
+
+# set defaults when declaring the parameters
+def be_cheerful(name='', repeat=2):
+	print(f"good morning {name}\n" * repeat)
+be_cheerful()    # output: good morning (repeated on 2 lines)
+be_cheerful("tim")    # output: good morning tim (repeated on 2 lines)
+be_cheerful(name="john")    # output: good morning john (repeated on 2 lines)
+be_cheerful(repeat=6)    # output: good morning (repeated on 6 lines)
+be_cheerful(name="michael", repeat=5)    # output: good morning michael (repeated on 5 lines)
+# NOTE: argument order doesn't matter if we are explicit when sending in our arguments!
+be_cheerful(repeat=3, name="kb")    # output: good morning kb (repeated on 3 lines)
+
