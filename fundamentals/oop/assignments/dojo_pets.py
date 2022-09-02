baf7f350ea0f42d64d26fe9dccb0d9936c8ebc83
John Fu <fu.johnyh@gmail.com>
2022-09-02 00:55:03 -0700

    partial dojo pets assignment

---
+++ fundamentals/oop/assignments/dojo_pets.py
@@ -0,0 +1,51 @@
+class Ninja:
+    # implement __init__( first_name , last_name , treats , pet_food , pet )
+    def __init__(self, first_name, last_name, treats, pet_food, pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+    
+    # implement the following methods:
+    # walk() - walks the ninja's pet invoking the pet play() method
+    def walk(self):
+        self.pet.play()
+        return self
+    # feed() - feeds the ninja's pet invoking the pet eat() method
+    def feed(self):
+        self.pet.eat()
+        return self
+    # bathe() - cleans the ninja's pet invoking the pet noise() method
+    def bathe(self):
+        self.pet.noise()
+        return self
+
+
+class Pet:
+    # implement __init__( name , type , tricks ):
+    def __init__(self, name, type, tricks, health, energy):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+
+    # implement the following methods:
+    # sleep() - increases the pets energy by 25
+    def sleep(self):
+        self.energy += 25
+        return self
+    # eat() - increases the pet's energy by 5 & health by 10
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        return self
+    # play() - increases the pet's health by 5
+    def play(self):
+        self.health += 5
+        return self
+    # noise() - prints out the pet's sound
+    def noise(self):
+        print(f'{self.name} is making {self.type} sounds.')
+        return self
--- fundamentals/oop/notes.py
+++ fundamentals/oop/notes.py
@@ -217,7 +217,38 @@ from my_modules import test_module
 __all__ = ["test_module"] # would only allow test_module to be imported
 
 
+#################################################
+# Polymorphism
 
+class Parent:
+    def method_a(self):
+        print("invoking PARENT method_a!")
+
+class Child(Parent):
+    def method_a(self):
+        print("invoking CHILD method_a!")
+
+dad = Parent()
+son = Child()
+dad.method_a()
+son.method_a() #notice this overrides the Parent method!
+
+
+# We'll use the Person class to demonstrate polymorphism
+# in which multiple classes inherit from the same class but behave in different ways
+class Person:
+    def pay_bill(self):
+        raise NotImplementedError
+
+# Millionaire inherits from Person
+class Millionaire(Person):
+    def pay_bill(self):
+        print("Here you go! Keep the change!")
+        
+# Grad Student also inherits from the Person class
+class GradStudent(Person):
+    def pay_bill(self):
+        print("Can I owe you ten bucks or do the dishes?")
 
 
 
