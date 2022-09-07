927fd3f61d1e6e8fc72a610fcf1bcbeeebe0173d
John Fu <fu.johnyh@gmail.com>
2022-09-06 22:56:05 -0700

    rendering with lists

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -22,6 +22,19 @@ def show_user_profile(username, id):
     print(id)
     return "username: " + username + ", id: " + id
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
+
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
