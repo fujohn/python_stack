48aea3b85e3b1c8ee6fe76f1a62aa307d8cfb4d4
John Fu <fu.johnyh@gmail.com>
2022-09-09 11:38:04 -0700

    nodes

--- fundamentals/oop/notes.py
+++ fundamentals/oop/notes.py
@@ -254,7 +254,7 @@ class GradStudent(Person):
 #################################################
 # Input and Outputs
 favorite_color = input('What is your favorite color? ') # input takes a prompt, which needs to be a string
-print(f'Your favorite color is: {favorite_color}') #output, prints the color given to the console
+print(f'Your favorite color is: {favorite_color}') #output, prints the color given to the console 😊
 
 
 
