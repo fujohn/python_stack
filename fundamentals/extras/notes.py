e9f38303be4b89bd410cc6a8441e650faa590722
John Fu <fu.johnyh@gmail.com>
2022-09-10 00:34:39 -0700

    multiple args

---
+++ fundamentals/extras/notes.py
@@ -0,0 +1,11 @@
+def varargs(arg1, *args):
+    print("Got ", arg1, " and ", args)
+varargs("one") 			# output: Got one and ()
+varargs("one", "two") 	        # output: Got one and ('two',)
+varargs("one", "two", "three")  # output: Got one and ('two', 'three')
+
+def varargs(arg1, *args):
+    for a in args:
+        print(a)
+varargs("one", "two", "three") # output: two, three (on separate lines)
+
