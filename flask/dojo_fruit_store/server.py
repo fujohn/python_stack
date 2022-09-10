52682f421322f2b162bb432198b5c271ab9294f8
John Fu <fu.johnyh@gmail.com>
2022-09-09 18:03:40 -0700

    dojo fruit store

--- flask/dojo_fruit_store/server.py
+++ flask/dojo_fruit_store/server.py
@@ -8,6 +8,9 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
+    fruit_count = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    name = request.form['first_name'] + ' ' + request.form['last_name']
+    print(f'Charging {name} for {fruit_count} fruits')
     return render_template("checkout.html")
 
 @app.route('/fruits')         
