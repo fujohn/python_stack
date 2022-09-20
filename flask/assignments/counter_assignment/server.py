fb1cd068dd360d350462a587400154b871b5542e
John Fu <fu.johnyh@gmail.com>
2022-09-20 14:08:35 -0700

    counter full minus style

--- flask/assignments/counter_assignment/server.py
+++ flask/assignments/counter_assignment/server.py
@@ -4,13 +4,16 @@ app.secret_key = 'q1w2e3r4t5y6' # set a secret key for security purposes
 
 @app.route('/')
 def index():
-    if 'counter' in session:
-        print('key exists!')
-        return render_template("index.html")
-    else:
-        print("key 'counter' does NOT exist")
+    if 'counter' not in session:
         session['counter'] = 0
-        return render_template("index.html")
+
+    if 'visited' not in session:
+        session['visited'] = 1
+    else:
+        session['visited'] += 1
+
+    return render_template('index.html')
+
 
 
 @app.route('/increase_counter', methods=['POST'])
@@ -32,9 +35,13 @@ def increase_counter_custom():
 
 @app.route('/destroy_session', methods=['POST'])
 def destroy_session():
-    # session.clear()		# clears all keys
-    session.pop('counter')		# clears a specific key
+    session.clear()		# clears all keys
+    # session.pop('counter')		# clears a specific key
     return redirect("/")	 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# import base64
+# base64.urlsafe_b64decode('eyJjb3VudGVyIjowLCJ2aXNpdGVkIjoxfQ===')
+
