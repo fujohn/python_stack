d45fac5568f86f9e4453bafa9fd0a806b4be7a0f
John Fu <fu.johnyh@gmail.com>
2022-09-20 17:57:03 -0700

    functional great number game

---
+++ flask/assignments/number_game_assignment/server.py
@@ -0,0 +1,28 @@
+from operator import methodcaller
+from flask import Flask, render_template, request, redirect, session
+import random # import the random module
+
+
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
+
+@app.route('/')
+def index():
+    if 'number' not in session:
+        session['number'] = random.randint(1, 100) # random number between 1-100
+    return render_template("index.html")
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    # Here we add two properties to session to store the name and email
+    session['guess'] = int(request.form['guess'])
+    return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
