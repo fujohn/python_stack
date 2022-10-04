7acd1938bc80cfb2b918ada2ba4a91238cd3afe5
John Fu <fu.johnyh@gmail.com>
2022-10-03 22:39:24 -0700

    recipes setup

--- flask_mysql/validation/burgers/flask_app/controllers/users.py
+++ flask_mysql/validation/burgers/flask_app/controllers/users.py
@@ -49,3 +49,17 @@ def login():
     session['user_id'] = user_in_db.id
     # never render on a post!!!
     return redirect("/dashboard")
+
+########################################### Associating logged in users #################################################
+# creating a tweet (not with codes above)
+@app.route('/tweet/create', methods=["POST"])
+def createTweet():
+    # Validations here...
+    data = {
+        "content": request.form['content'],
+        "location": request.form['location'],
+        # Pass in the id of the User in session to use as the foreign key to describe who made the Tweet
+        "user_id": session['user_id']
+    }
+    Tweet.save(data)
+    # ...
--- flask_mysql/validation/burgers/flask_app/models/user.py
+++ flask_mysql/validation/burgers/flask_app/models/user.py
@@ -34,3 +34,63 @@ class User:
         if len(result) < 1:
             return False
         return cls(result[0])
+
+##################################### associating users in classes
+
+# tweet related
+from twitter_app.models import tweet
+# Other imports here...
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.handle = data['handle']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        # [] can represent a currently empty place to store all of the tweets that a single User instance has created, as a User creates MANY Tweets
+        self.tweets = [] 
+
+from twitter_app.models import user
+# Other imports here...
+class Tweet:
+    def __init__(self, data):
+        self.id = data['id']
+        self.content = data['content']
+        self.location = data['location']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at'] 
+        # None can represent a currently empty space for a single User dictionary to be placed here, as a Tweet is made by ONE User. We want a User instance and all their attributes to be placed here, so something like data['...'] will not work as we have to make the User instance ourselves.
+        self.creator = None
+    
+    @classmethod
+    def get_all_tweets_with_creator(cls):
+        # Get all tweets, and their one associated User that created it
+        query = "SELECT * FROM tweets JOIN users ON tweets.user_id = users.id;"
+        results = connectToMySQL('real_twitter').query_db(query)
+        all_tweets = []
+        for row in results:
+            # Create a Tweet class instance from the information from each db row
+            one_tweet = cls(row)
+            # Prepare to make a User class instance, looking at the class in models/user.py
+            one_tweets_author_info = {
+                # Any fields that are used in BOTH tables will have their name changed, which depends on the order you put them in the JOIN query, use a print statement in your classmethod to show this.
+                "id": row['users.id'], 
+                "name": row['name'],
+                "handle": row['handle'],
+                "email": row['email'],
+                "password": row['password'],
+                "created_at": row['users.created_at'],
+                "updated_at": row['users.updated_at']
+            }
+            # Create the User class instance that's in the user.py model file
+            author = user.User(one_tweets_author_info)
+            # Associate the Tweet class instance with the User class instance by filling in the empty creator attribute in the Tweet class
+            one_tweet.creator = author
+            # Append the Tweet containing the associated User to your list of tweets
+            all_tweets.append(one_tweet)
+        return all_tweets
+
+
+
