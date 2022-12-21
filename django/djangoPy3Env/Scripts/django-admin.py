34b96ca10ab7701f8e8772a01d35546a23b6354c
John Fu <fu.johnyh@gmail.com>
2022-12-20 21:10:48 -0800

    .

---
+++ django/djangoPy3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\johnf\OneDrive\Desktop\coding\codingdojo\python_stack\django\djangoPy3Env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
