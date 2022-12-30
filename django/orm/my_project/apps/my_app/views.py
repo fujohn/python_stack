02d522dd9a08610d9ccf82e7196a4f0f815655db
John Fu <fu.johnyh@gmail.com>
2022-12-29 21:14:58 -0800

    .

--- django/orm/my_project/apps/my_app/views.py
+++ django/orm/my_project/apps/my_app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+def index(request):
+    return HttpResponse('placeholder to later display a list of all blogs')
