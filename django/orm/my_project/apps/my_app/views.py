f0130067a482c3177496a6dfece879814267dc31
John Fu <fu.johnyh@gmail.com>
2022-12-30 23:41:03 -0800

    django orm

--- django/orm/my_project/apps/my_app/views.py
+++ django/orm/my_project/apps/my_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect
+# other imports
+from .models import Movie
 
-# Create your views here.
+# show all of the data from a table
 def index(request):
-    return HttpResponse('placeholder to later display a list of all blogs')
+    context = {
+        "all_the_movies": Movie.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def index_2(request): # for one to many
+    context = {"authors": Author.objects.all()}		# we're only sending up all the authors
+    return render(request, "index.html", context)
+
