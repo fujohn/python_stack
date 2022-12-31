da413ce5b8c72627f4ac87ae085a86394eb190cd
John Fu <fu.johnyh@gmail.com>
2022-12-30 23:40:44 -0800

    django orm

--- django/orm/my_project/apps/my_app/models.py
+++ django/orm/my_project/apps/my_app/models.py
@@ -11,3 +11,25 @@ class Movie(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+# one to many
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, related_name="books", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# many to many (in relation to book)
+# second of many to many class in terms of line order will always have the ManyToManyField method 
+##  (if used in books, it will have to be below publisher)
+class Publisher(models.Model):
+	name = models.CharField(max_length=255)
+	books = models.ManyToManyField(Book, related_name="publishers")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
