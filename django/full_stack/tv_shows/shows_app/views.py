086c76aa557f591413cc64c82ecb0f5bc53644ee
John Fu <fu.johnyh@gmail.com>
2023-01-03 17:37:35 -0800

    user-dashboard project

--- django/full_stack/tv_shows/shows_app/views.py
+++ django/full_stack/tv_shows/shows_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Show
+import bcrypt
 
 # Create your views here.
 def index(request):
@@ -68,3 +69,37 @@ def destroy(request, show_id):
     show_to_delete = Show.objects.get(id=show_id)
     show_to_delete.delete()
     return redirect('/shows/')
+
+# def validate_login(request):
+#     user = User.objects.get(email=request.POST['email'])  # hm...is it really a good idea to use the get method here?
+#    if bcrypt.checkpw(request.POST['password'].encode(), user.pw_hash.encode()):
+#         print("password match")
+#     else:
+#         print("failed password")
+
+def register(request):    
+    # include some logic to validate user input before adding them to the database!
+    password = request.POST['password']
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()  # create the hash    
+    print(pw_hash)      # prints something like b'$2b$12$sqjyok5RQccl9S6eFLhEPuaRaJCcH3Esl2RWLm/cimMIEnhnLb7iC'    
+    # be sure you set up your database so it can store password hashes this long (60 characters)
+    # make sure you put the hashed password in the database, not the one from the form!
+    User.objects.create(username=request.POST['username'], password=pw_hash) 
+    return redirect("/") # never render on a post, always redirect!    
+
+def login(request):
+    # see if the username provided exists in the database
+    user = User.objects.filter(username=request.POST['username']) # why are we using filter here instead of get?
+    if user: # note that we take advantage of truthiness here: an empty list will return false
+        logged_user = user[0]
+        # assuming we only have one user with this username, the user would be first in the list we get back
+        # of course, we should have some logic to prevent duplicates of usernames when we create users
+        # use bcrypt's check_password_hash method, passing the hash from our database and the password from the form
+        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+            # if we get True after checking the password, we may put the user id in session
+            request.session['userid'] = logged_user.id
+            # never render on a post, always redirect!
+            return redirect('/success')
+    # if we didn't find anything in the database by searching by username or if the passwords don't match, 
+    # redirect back to a safe route
+    return redirect("/")
